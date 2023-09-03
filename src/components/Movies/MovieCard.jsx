76472f6de98d4c4c17b3492a88520237d4684694
KatanES <businesskatan.es@gmail.com>
2023-09-03 16:27:34 +0200

    location deleted

--- src/components/Movies/MovieCard.jsx
+++ src/components/Movies/MovieCard.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import { Location } from 'react-router-dom';
+// import { Location } from 'react-router-dom';
 export const MovieCard = ({ movieList }) => {
   return (
     <main>
