b9566676165fe3a94746e312cfe757b78cef0a9c
KatanES <businesskatan.es@gmail.com>
2023-09-14 18:41:37 +0200

    ArrowBack FINAL

--- src/components/Movies/MovieCard.jsx
+++ src/components/Movies/MovieCard.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import {
   SearchMovieList,
   SearchMovieItem,
@@ -9,20 +9,21 @@ import {
 const imgBaseUrl = 'https://image.tmdb.org/t/p/w300';
 
 export const MovieCard = ({ movieList }) => {
+  const location = useLocation();
   return (
     <SearchPageContainer>
       <SearchPageTitle>Search results</SearchPageTitle>
       <SearchMovieList>
         {movieList.map(movie => (
           <SearchMovieItem key={movie.id}>
-            <NavLink to={`/movies/${movie.id}`}>
+            <Link to={`/movies/${movie.id}`} state={{ from: location }}>
               <SearchPoster
                 src={imgBaseUrl + movie.poster_path}
                 width={140}
                 alt={movie.original_title}
               />
               {movie.original_title || movie.name}
-            </NavLink>
+            </Link>
           </SearchMovieItem>
         ))}
       </SearchMovieList>
